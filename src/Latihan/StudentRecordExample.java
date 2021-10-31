9c709ca66170cf38ae27847b612bd066f518bcc7
bintangahmadafarhan <adamabintang@gmail.com>
2021-10-31 08:36:15 +0700

    create modul4

---
+++ src/Latihan/StudentRecordExample.java
@@ -0,0 +1,20 @@
+package Latihan;
+
+//20104020
+//Biintang Ahmada Farhan Adama
+
+public class StudentRecordExample {
+    public static void main( String[] args ){
+        StudentRecord Wahyu = new StudentRecord();
+        StudentRecord Ini = new StudentRecord();
+        StudentRecord Nama = new StudentRecord();
+        Wahyu.setName("Wahyu");
+        Ini.setName("Ini");
+        Nama.setName("Nama");
+        System.out.println(Wahyu.getName() );
+        System.out.println("Hitung="+StudentRecord.getStudentCount
+                ()
+        );
+    }
+
+}
---
+++ src/Percobaan/main.java
@@ -0,0 +1,19 @@
+package Percobaan;
+
+public class main {
+    public static void main(String[] args) {
+
+            Tabungan saya = new Tabungan(
+                    "Barca",
+                    50410420,
+                    1_000_000,
+                    12345
+            );
+
+            System.out.println("Nama        : " + saya.getNama());
+            System.out.println("No Rekening : " + saya.getNoRekening());
+            System.out.println("Saldo       : " + saya.getSaldo());
+            System.out.println("Pin         : " + saya.getPin());
+        }
+    }
+
